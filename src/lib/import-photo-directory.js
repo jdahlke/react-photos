ad31b334f0f5657dfd685ddbfd333e67c47194ef
Joergen Dahlke <joergen.dahlke@infopark.de>
2020-12-28 19:05:09 +0100

    [fix] no presentation.yml

--- src/lib/import-photo-directory.js
+++ src/lib/import-photo-directory.js
@@ -3,28 +3,25 @@
 import yaml from 'js-yaml'
 
 import Photos from 'src/app/photos'
+import initEvent from 'src/lib/init-event'
 
 export default function importPhotoDirectory (event) {
   const files = Array.from(event.target.files)
-  let presentationFile
 
   files.forEach(file => {
     if (file.type === 'image/jpeg') {
       Photos.add(file.name, URL.createObjectURL(file))
     } else if (file.name === 'presentation.yml') {
-      presentationFile = file
+      const reader = new window.FileReader()
+      reader.onload = event => {
+        const presentation = yaml.safeLoad(event.target.result)
+        Photos.configure(presentation)
+        initEvent('photos:start')
+      }
+      reader.onerror = error => {
+        throw error
+      }
+      reader.readAsText(file)
     }
   })
-
-  if (presentationFile) {
-    const reader = new window.FileReader()
-    reader.onload = event => {
-      const presentation = yaml.safeLoad(event.target.result)
-      Photos.configure(presentation)
-    }
-    reader.onerror = error => {
-      throw error
-    }
-    reader.readAsText(presentationFile)
-  }
 }
